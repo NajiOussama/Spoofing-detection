e2dba04fefe8e43f0636b26020036d86476d2d8a
Oussama Naji <oussama.naji@student-cs.fr>
2023-03-22 12:04:43 +0100

    'maj3 de FAS_dataset.py'

--- dataset/FAS_dataset.py
+++ dataset/FAS_dataset.py
@@ -6,13 +6,32 @@ import numpy as np
 from PIL import Image
 from torchvision import transforms
 import glob
+
+class NonDistortedAugmentation:
+    def __init__(self, image_size, mean, std):
+        self.image_size = image_size
+        self.mean = mean
+        self.std = std
+        self.transform = transforms.Compose([
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomRotation(degrees=15),
+            transforms.RandomCrop(size=self.image_size),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=self.mean, std=self.std)
+        ])
+
+    def __call__(self, img):
+        return self.transform(img)
+
 class FASDataset(Dataset):
 
     def __init__(self, root_dir, csv_file, transform=None, is_train=True, smoothing=True):
         super().__init__()
         self.root_dir = root_dir
         self.data = pd.read_csv(os.path.join(self.root_dir, csv_file))
-        self.transform = transform or NonDistortedAugmentation((224, 224), [0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+        self.mean = [0.485, 0.456, 0.406]
+        self.std = [0.229, 0.224, 0.225]
+        self.transform = transform or NonDistortedAugmentation((224, 224), self.mean, self.std)
         
         if smoothing:
             self.label_weight = 1.0
