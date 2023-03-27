1b188edb46cb4ca4cdd016c203db585d1c00a6be
Oussama Naji <oussama.naji@student-cs.fr>
2023-03-27 14:25:43 +0200

    'j'ai remis le FASdataset.py par défaut'

--- dataset/FAS_dataset.py
+++ dataset/FAS_dataset.py
@@ -7,26 +7,18 @@ from PIL import Image
 from torchvision import transforms
 import glob
 
-
-class NonDistortedAugmentation:
-    def __init__(self, img_size=(224, 224), mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]):
-        self.transform = transforms.Compose([
-            transforms.Resize(img_size),
-            transforms.ToTensor(),
-            transforms.Normalize(mean, std)
-        ])
-
-    def __call__(self, img):
-        return self.transform(img.convert('RGB'))
 class FASDataset(Dataset):
-    
 
     def __init__(self, root_dir, csv_file, transform=None, is_train=True, smoothing=True):
         super().__init__()
         self.root_dir = root_dir
         self.data = pd.read_csv(os.path.join(self.root_dir, csv_file))
-        self.transform = transform or NonDistortedAugmentation((224, 224), [0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
-        self.label_weight = 1.0 if smoothing else 0.99
+        self.transform = transform
+        
+        if smoothing:
+            self.label_weight = 1.0
+        else:
+            self.label_weight = 0.99
 
     def __getitem__(self, index):
         img_name = self.data.iloc[index, 0]
@@ -37,8 +29,9 @@ class FASDataset(Dataset):
         label = label.astype(np.float32)
         label = np.expand_dims(label, axis=0)
         
-        img1 = self.transform(img)
-        img2 = self.transform(img)
+        if self.transform:
+            img1 = self.transform(img)
+            img2 = self.transform(img)
 
         return img1, img2, label
 
